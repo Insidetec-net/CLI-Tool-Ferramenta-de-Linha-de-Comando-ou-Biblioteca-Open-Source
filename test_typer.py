0731b9762af6c217b55b3a6af4adfa557c781ea9
Emerson Pires <mac@iMac-de-Emerson.local>
2026-09-04 14:17:11 -0700

    chore: add gitignore and clean cache files

--- test_typer.py
+++
@@ -1,9 +0,0 @@
-from typer.testing import CliRunner
-from arch_forge.main import app
-
-runner = CliRunner()
-result = runner.invoke(app, ["init", "meu-projeto"])
-print(result.exit_code)
-print(result.output)
-if result.exception:
-    print(result.exception)
